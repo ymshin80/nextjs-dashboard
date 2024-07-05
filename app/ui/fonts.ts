6de4feba83c8bdda237362fbb331d0a098eadefd
ymshin80 <siyumy80@gmail.com>
2024-07-05 13:58:58 +0900

    챕터5까지

--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -1,6 +1,10 @@
-import { Lusitana  } from 'next/font/google';
+import { Lusitana, Inter  } from 'next/font/google';
  
 export const lusitana = Lusitana({
   weight: ['400', '700'],
   subsets: ['latin'],
+});
+
+export const inter = Inter({
+  subsets: ['latin']
 });

4d0d2d4acbbe12257091fcabdf94cdd1ab5b645f
ymshin80 <siyumy80@gmail.com>
2024-07-11 15:17:18 +0900

    ppr 주석

--- auth.ts
+++ auth.ts
@@ -28,13 +28,13 @@ export const { auth, signIn, signOut } = NextAuth({
                 if(parsedCredentials .success) {
                     const { email, password } = parsedCredentials.data;
                     const user = await getUser(email);
-
                     if(!user) return null;
+
                     const passwordsMatch = await bcrypt.compare(password, user.password);
 
                     if(passwordsMatch) return user;
                 }
-                console.log('Invalid credentials');
+                 console.log('Invalid credentials');
                 return null;
             },
             
